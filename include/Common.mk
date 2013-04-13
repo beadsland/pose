1e0c40c54cd34350c13e86e7711b0d6d214bd95f
beadsland <beads.d.land@gmail.com>
2013-04-12 22:22:57 -0400

    backups working for unison merge

--- include/Common.mk
+++ include/Common.mk
@@ -28,7 +28,7 @@ include include/Header.mk
 # All good rules
 #
 
-.PHONY:	all good todo docs compile neat current clean push make
+.PHONY:	all good todo docs compile neat current clean push make test
 
 all:		push compile good
 
@@ -52,11 +52,13 @@ todo:		README.md
 README.md:	neat doc/TODO_head.edoc
 	@$(CROWBAR:_cmds_=doc)
 
-doc/TODO_head.edoc:
-	if [ $(TODO_MORE) -gt 0 ]; \
+doc/TODO_head.edoc:		TODO.edoc
+	@if [ $(TODO_MORE) -gt 0 ]; \
 		then (head -7 TODO.edoc; \
 			  echo "@todo ...plus $(TODO_MORE) more (see TODO.edoc)"); fi \
 		> doc/TODO_head.edoc
+
+TODO.edoc:	;
 
 #
 # Rules for compiling 
@@ -72,12 +74,12 @@ neat:
 # Rules for managing dependencies
 #
 
-current:	pose
+current:
 	@if [ "$(ONLINE)" == yes ]; \
 		then $(CROWBAR:_cmds_=update-deps compile doc); \
 		else $(CROWBAR:_cmds_=compile doc); fi
 
-clean:		pose
+clean:
 	@if [ "$(ONLINE)" == yes ]; \
 		then $(CROWBAR:_cmds_=delete-deps clean get-deps); \
 		else $(CROWBAR:_cmds_=clean); fi
@@ -86,9 +88,19 @@ clean:		pose
 # Rules for managing revisions and synchronized common files
 #
 
-push:		make
+push:	make
 	@if [ "$(DEV)" == yes -a "$(ONLINE)" == yes ]; \
 		then (git push origin master); fi
+
+test:	$(patsubst include/%.mk, \
+			include/$(B_PREFIX)%.mk$(B_SUFFIX), \
+			$(wildcard include/*.mk))
+	@if [ "$(shell basename $(CURDIR))" != nosh ]; \
+		then ($(UNISON) -merge "$(MERGE)"); fi
+
+include/$(B_PREFIX)%.mk$(B_SUFFIX):		include/%.mk
+	@if [ "$(shell basename $(CURDIR))" != nosh ]; \
+		then ($(UNISON) && (test -f $@ || cp $< $@)); fi
 
 make:
 	@if [ "$(shell basename $(CURDIR))" != nosh ]; \
